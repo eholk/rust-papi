4ea64e14acd921ae66bc78b4f2fdf27d1c7f64d3
Eric Holk <eric.holk@gmail.com>
2013-10-10 19:22:03 -0400

    Added some basic instructions in a README.

--- src/papi/main.rs
+++ src/papi/main.rs
@@ -1,19 +1,17 @@
 extern mod papi;
 
+fn main() {
+    let counters = papi::CounterSet::new([papi::PAPI_TOT_INS]);
+
+    let start = counters.read();
+    let x = fib(14);
+    let stop = counters.accum();
+
+    println!("Computed fib(14) = {:d} in {:d} instructions.",
+             x, stop[0] - start[0]);
+}
+
 fn fib(n: int) -> int {
     if n < 2 { 1 }
     else { fib(n - 1) + fib(n - 2) }
-}
-
-fn main() {
-    println!("Found {:d} counters.", papi::num_counters());
-
-    println!("\nComputing some stuff...");
-    papi::start_counters([papi::PAPI_TOT_INS, papi::PAPI_TOT_CYC]);
-    let x = fib(14);
-    let values = [0, 0];
-    papi::read_counters(values);
-    println!("fib(14) = {:d}", x);
-    println!("Executed {:d} instructions in {:d} cycles ({:f} IPC)",
-             values[0], values[1], values[0] as f64 / values[1] as f64);
 }
